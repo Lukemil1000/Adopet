27a8ca7eca256e93ee66b98617a8ec97ccf1b3ef
Lucas Pomar <lucaspomar@outlook.com>
2024-09-18 19:36:38 -0300

    tutors endpoint

--- adopet/urls.py
+++ adopet/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
+from adopet.views import TutorViewSet
 
 router = routers.DefaultRouter()
+router.register("tutors", TutorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- adopet/views.py
+++ adopet/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from adopet.models import Tutor
+from adopet.serializers import TutorSerializer
 
-# Create your views here.
+class TutorViewSet(viewsets.ModelViewSet):
+    """Endpoint para o modelo de Tutor"""
+
+    queryset = Tutor.objects.all()
+    serializer_class = TutorSerializer

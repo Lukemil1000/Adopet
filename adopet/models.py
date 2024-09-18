d3b6767e6cfefb2471c94a44b7e10275fa22b490
Lucas Pomar <lucaspomar@outlook.com>
2024-09-18 18:26:51 -0300

    tutor model

--- adopet/models.py
+++ adopet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tutor(models.Model):
+    """Modelo de usuário para um tutor"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(blank=True, max_length=11)
+    city = models.CharField(blank=True, max_length=50)
+    about = models.TextField(blank=True)
+    # picture = models.ImageField(null=True)
+    # adoptions

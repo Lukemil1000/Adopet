febf4ca27b2448339b9fcc4bd5671da61a934932
Lucas Pomar <lucaspomar@outlook.com>
2024-09-26 15:46:44 -0300

    Shelter viewset

--- adopet/views.py
+++ adopet/views.py
@@ -1,9 +1,15 @@
 from rest_framework import permissions, viewsets
-from adopet.models import Tutor
-from adopet.serializers import TutorSerializer
+from adopet.models import Tutor, Shelter
+from adopet.serializers import TutorSerializer, ShelterSerializer
 
 class TutorViewSet(viewsets.ModelViewSet):
     """Endpoint para o modelo de Tutor"""
 
     queryset = Tutor.objects.all()
     serializer_class = TutorSerializer
+
+class ShelterViewSet(viewsets.ModelViewSet):
+    """Endpoint para o modelo de Abrigo"""
+
+    queryset = Shelter.objects.all()
+    serializer_class = ShelterSerializer

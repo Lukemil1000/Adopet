8f772a5bfd26acc83db1b9541041e398610a037a
Lucas Pomar <lucaspomar@outlook.com>
2024-09-26 16:00:38 -0300

    shelter post_delete user

--- adopet/signals.py
+++ adopet/signals.py
@@ -1,8 +1,9 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_delete
-from adopet.models import Tutor
+from adopet.models import Tutor, Shelter
 
 @receiver(post_delete, sender=Tutor)
+@receiver(post_delete, sender=Shelter)
 def post_delete_user(sender, instance, *args, **kwargs):
-    if instance.user: # just in case user is not specified
+    if instance.user: 
         instance.user.delete()

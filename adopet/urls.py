53e20df7d6177901ea7314b7d2db44b04d75db83
Lucas Pomar <lucaspomar@outlook.com>
2024-09-26 15:47:40 -0300

    Create shelters endpoint

--- adopet/urls.py
+++ adopet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from adopet.views import TutorViewSet
+from adopet.views import TutorViewSet, ShelterViewSet
 
 router = routers.DefaultRouter()
 router.register("tutors", TutorViewSet)
+router.register("shelters", ShelterViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),

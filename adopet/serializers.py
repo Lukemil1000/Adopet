e842a4cfc3102fe7b97f3eba712241ba7a9ac230
Lucas Pomar <lucaspomar@outlook.com>
2024-09-26 15:32:22 -0300

    Shelter serializer

--- adopet/serializers.py
+++ adopet/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from drf_writable_nested.mixins import UniqueFieldsMixin
-from adopet.models import Tutor
+from adopet.models import Shelter, Tutor
 
 class UserSerializer(UniqueFieldsMixin, serializers.ModelSerializer):
     class Meta:
@@ -49,3 +49,31 @@ class TutorSerializer(serializers.ModelSerializer):
         if not city.isalpha():
             raise serializers.ValidationError('City deve conter apenas letras')
         return city
+    
+class ShelterSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Shelter
+        fields = ["id", "user", "cnpj", "name", "phone", "adress", "about"]
+    
+    def create(self, validated_data):
+        user_data = validated_data.pop("user")
+        user = User.objects.create(**user_data)
+        shelter = Shelter.objects.create(user=user, **validated_data)
+        return shelter
+    
+    def update(self, instance, validated_data):
+        if "user" in validated_data.keys():
+            user_data = validated_data.pop("user")
+            instance.user.username = user_data.get("username", instance.user.username)
+            instance.user.password = user_data.get("password", instance.user.password)
+            instance.user.email = user_data.get("email", instance.user.email)
+            instance.user.save()
+        instance.cnpj = validated_data.get("cnpj", instance.cnpj)
+        instance.name = validated_data.get("name", instance.name)
+        instance.phone = validated_data.get("phone", instance.phone)
+        instance.adress = validated_data.get("adress", instance.adress)
+        instance.about = validated_data.get("about", instance.about)
+        instance.save()
+        return instance

9791c5406d53024348727b409fd13b4465131ad9
Lucas Pomar <lucaspomar@outlook.com>
2024-10-11 15:40:36 -0300

    Remove TutorAdmin

--- adopet/admin.py
+++ adopet/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 from adopet.models import Tutor
 
-class TutorAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "phone", "city")
-    list_per_page = 10
-
-admin.site.register(Tutor, TutorAdmin)
